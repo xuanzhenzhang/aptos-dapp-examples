68f9b28bcc49a245a449a10e89397e707b9693d7
xuanzhenzhang <xuanzhenzhang6@gmail.com>
2022-09-18 01:21:08 -0700

    dapp setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Types, AptosClient } from "aptos";
+
+const client = new AptosClient("https://fullnode.devnet.aptoslabs.com/v1");
+
+function stringToHex(text: string) {
+  const encoder = new TextEncoder();
+  const encoded = encoder.encode(text);
+  return Array.from(encoded, (i) => i.toString(16).padStart(2, "0")).join("");
+}
+
+function hexToString(text: string) {
+  var hex = text.toString();
+  var str = "";
+  for (var n = 0; n < hex.length; n += 2) {
+    str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+  }
+  return str;
+}
 
 function App() {
+  // Retrieve aptos.account on initial render and store it.
+  const [address, setAddress] = React.useState<string | null>(null);
+  React.useEffect(() => {
+    window.aptos
+      .account()
+      .then((data: { address: string }) => setAddress(data.address));
+  }, []);
+
+  // Use the AptosClient to retrieve details about the account.
+  const [account, setAccount] = React.useState<Types.AccountData | null>(null);
+  React.useEffect(() => {
+    if (!address) return;
+    client.getAccount(address).then(setAccount);
+  }, [address]);
+
+  // Check for the module; show publish instructions if not present.
+  const [modules, setModules] = React.useState<Types.MoveModuleBytecode[]>([]);
+  React.useEffect(() => {
+    if (!address) return;
+    client.getAccountModules(address).then(setModules);
+  }, [address]);
+
+  const hasModule = modules.some((m) => m.abi?.name === "message");
+  const publishInstructions = (
+    <pre>
+      Run this command to publish the module:
+      <br />
+      aptos move publish --package-dir /path/to/hello_blockchain/
+      --named-addresses HelloBlockchain={address}
+    </pre>
+  );
+
+  // Call set_message with the textarea value on submit.
+  const ref = React.createRef<HTMLTextAreaElement>();
+  const [isSaving, setIsSaving] = React.useState(false);
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    if (!ref.current) return;
+
+    const message = ref.current.value;
+    const transaction = {
+      type: "entry_function_payload",
+      function: `${address}::message::set_message`,
+      arguments: [stringToHex(message)],
+      type_arguments: [],
+    };
+
+    try {
+      setIsSaving(true);
+      await window.aptos.signAndSubmitTransaction(transaction);
+    } finally {
+      setIsSaving(false);
+    }
+  };
+
+  // Get the message from account resources.
+  const [resources, setResources] = React.useState<Types.MoveResource[]>([]);
+  React.useEffect(() => {
+    if (!address) return;
+    client.getAccountResources(address).then(setResources);
+  }, [address]);
+  const resourceType = `${address}::message::MessageHolder`;
+  const resource = resources.find((r) => r.type === resourceType);
+  const data = resource?.data as { message: string } | undefined;
+  const message = data?.message;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {hasModule ? (
+        <form onSubmit={handleSubmit}>
+          <textarea ref={ref} defaultValue={message && hexToString(message)} />
+          <input disabled={isSaving} type='submit' />
+        </form>
+      ) : (
+        publishInstructions
+      )}
     </div>
   );
 }
